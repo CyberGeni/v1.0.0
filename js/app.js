a0870222e40c2f14404b151839a7df6f8227f0fc
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2021-11-08 17:02:54 +0100

    added particle.js config

---
+++ js/app.js
@@ -0,0 +1,9 @@
+//  Particles Init
+
+window.onload= function() {
+  Particles.init ({
+    selector: '.background',
+    connectParticles: true,
+    color: ['#DA0463', '#404B69', '#DBEDF3'],
+  });
+};
