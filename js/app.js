a2395acd2c4daa3cfa1fd853bc4b0cf816b35256
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2021-11-12 21:01:03 +0100

    added a little bg tweaks

--- js/app.js
+++ js/app.js
@@ -1,9 +1,21 @@
-//  Initiate Particles Library
+//  Initiate Particles.js Library
 
 window.onload= function() {
-  Particles.init ({
+Particles.init ({
     selector: '.background',
-    connectParticles: false,
+    connectParticles: true,
     color: ['#DA0463', '#404B69', '#DBEDF3'],
+    maxParticles: 75,
+    minDistance: 150,
+    speed: 0.7,
   });
 };
+
+// Initialise Typed.js
+
+var typed = new Typed('#typed', {
+  stringsElement: '#typed-strings',
+  smartBackspace: true,
+});
+
+
